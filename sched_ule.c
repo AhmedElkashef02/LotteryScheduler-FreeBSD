6a1682b0bc803f6a91148e3d0306c178fe50cdcc
Ahmed Salah Eldin Elkashef <ahmedelkashef2012@gmail.com>
2018-03-20 11:24:40 +0200

    pass the process to increase/decrease functions

--- sched_ule.c
+++ sched_ule.c
@@ -1530,8 +1530,8 @@ sched_interact_score(struct thread *td)
 
 //Increases the number of tickets
 static void
-sched_increaseTickets(struct thread *td, int score) {
-	int new_num_tickets = td->tickets + 50*(100-score);
+sched_increaseTickets(struct thread *td, struct proc *p, int score) {
+	int new_num_tickets = td->tickets + 1;
 	if(new_num_tickets <= 1) {
 		new_num_tickets = 1;
 	} else if (new_num_tickets >= 100000) {
@@ -1542,8 +1542,8 @@ sched_increaseTickets(struct thread *td, int score) {
 
 //Decreases the number of tickets
 static void
-sched_decreaseTickets(struct thread *td, int score) {
-	int new_num_tickets = td->tickets - 50*(score);
+sched_decreaseTickets(struct thread *td, struct proc *p, int score) {
+	int new_num_tickets = td->tickets - 1;
 	if(new_num_tickets <= 1) {
 		new_num_tickets = 1;
 	} else if(new_num_tickets >= 10000) {
@@ -2056,9 +2056,9 @@ sched_nice(struct proc *p, int nice)
 		sched_priority(td);
 		sched_prio(td, td->td_base_user_pri);
 		if(nice < 0) {
-			sched_increaseTickets(td, (100+nice));
+			sched_increaseTickets(td, p, (100+nice));
 		} else {
-			sched_decreaseTickets(td, nice);
+			sched_decreaseTickets(td, p, nice);
 		}
 		thread_unlock(td);
 	}
