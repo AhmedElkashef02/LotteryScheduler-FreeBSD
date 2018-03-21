5e88857e03548bb0a14cc27e2b9a1c629f8b0fed
Ahmed Salah Eldin Elkashef <ahmedelkashef2012@gmail.com>
2018-03-21 23:50:12 +0200

    Update sys_gift.c

--- sys_gift.c
+++ sys_gift.c
@@ -18,21 +18,35 @@ int b;
 int
 sys_gift(pid_t p_pid, int tickets)
 {
-  struct proc *target_p = pfind(p_pid);
-  struct proc *this_p = pfind(getpid());
+  struct proc *this_p = pfind(getpid()); //1)defines the current process, 2)locks it
+  struct thread *td;
+  
   printf("before transfer: this process: %d", this_p->total_tickets);
-  printf("before transfer: target process: %d", target_p->total_tickets);
-  // check if this process can transfer
-  if (this_p->total_tickets - tickets >= 1 && target_p->total_tickets + tickets <= 100000) {
-    // transfer the tickets
-    this_p->total_tickets -= tickets;
-    target_p->total_tickets += tickets;
+  
+  if(p_pid == 0 && tickets == 0) {
+    td->td_retval[0] = this_p->total_tickets;
+    PROC_UNLOCK(this_p);
   } else {
-    // do not transfer, and print out the tickets of this_p
-    printf("total tickets for invoking process is: %d\n", this_p->total_tickets); 
+    // check that tickets is a valid number
+    if (tickets > 0) {
+      struct proc *target_p = pfind(p_pid);
+      // check if its a valid process
+      if (target_p == NULL) {
+        PROC_UNLOCK(target_p);
+        td->td_retval[0] = 0;
+      } else {
+        printf("before transfer: target process: %d", target_p->total_tickets);
+        // check if this process can transfer
+        if (this_p->total_tickets - tickets >= 1 && target_p->total_tickets + tickets <= 100000) {
+          // transfer the tickets
+          sched_increaseTickets(target_p, tickets);
+          sched_decreaseTickets(this_p, tickets);
+          printf("after transfer: this process: %d", this_p->total_tickets);
+          printf("after transfer: target process: %d", target_p->total_tickets);
+        }
+      }
+    }
   }
-  printf("after transfer: this process: %d", this_p->total_tickets);
-  printf("after transfer: target process: %d", target_p->total_tickets);
 return 0;
 }
 
