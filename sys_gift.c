f3f43fbf67b3314d55edcf5989309cb9e7553e36
Ahmed Salah Eldin Elkashef <ahmedelkashef2012@gmail.com>
2018-03-21 16:58:24 +0200

    testing - gift system call

--- sys_gift.c
+++ sys_gift.c
@@ -20,6 +20,8 @@ sys_gift(pid_t p_pid, int tickets)
 {
   struct proc *target_p = pfind(p_pid);
   struct proc *this_p = pfind(getpid());
+  printf("before transfer: this process: %d", this_p->total_tickets);
+  printf("before transfer: target process: %d", target_p->total_tickets);
   // check if this process can transfer
   if (this_p->total_tickets - tickets >= 1 && target_p->total_tickets + tickets <= 100000) {
     // transfer the tickets
@@ -29,6 +31,8 @@ sys_gift(pid_t p_pid, int tickets)
     // do not transfer, and print out the tickets of this_p
     printf("total tickets for invoking process is: %d\n", this_p->total_tickets); 
   }
+  printf("after transfer: this process: %d", this_p->total_tickets);
+  printf("after transfer: target process: %d", target_p->total_tickets);
 return 0;
 }
 
