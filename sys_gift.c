4c2539b7b9ea235ebf247e2468f77fbe441f291a
Ahmed Salah Eldin Elkashef <ahmedelkashef2012@gmail.com>
2018-03-28 19:42:38 +0200

    Update sys_gift.c

--- sys_gift.c
+++ sys_gift.c
@@ -10,77 +10,77 @@
 #include <sys/mutex.h>
 
 #ifndef _SYS_SYSPROTO_H_
-struct gift_args{
-int p_pid;
-int tickets;
+struct gift_args {
+        int p_pid;
+        int tickets;
 };
 #endif
 
 int
 sys_gift(struct thread *td, struct gift_args *args)
 {
-  int p_pid;
-  int tickets;
-  int total_tickets_per_proc = 0;
-  int total_tickets_per_targ = 0;
-  
-  p_pid = args->p_pid;
-  tickets = args->tickets;
-  
-  struct proc *this_p = td->td_proc; //1)defines the current process
-  PROC_LOCK(this_p);
-  
-  FOREACH_THREAD_IN_PROC(this_p, td) {
-		thread_lock(td);	
-		total_tickets_per_proc += td->tickets;
-		thread_unlock(td);
-	}
-  
-  printf("before transfer: this process: %d\n", total_tickets_per_proc);
-  
-  // if gift(0,0);
-  if(p_pid == 0 && tickets == 0) {
-    td->td_retval[0] = total_tickets_per_proc;
-    PROC_UNLOCK(this_p);
-  } else {
-    // check that tickets is a valid number
-    if (tickets > 0) {
-      struct proc *target_p = pfind(p_pid);
-	    
-      // check if the process is existent
-      if (pfind(p_pid) == NULL) {
-	PROC_UNLOCK(target_p);
-	PROC_UNLOCK(this_p);
-	td->td_retval[0] = 1;
-	      printf("Error(): The process does not exist\n");
-      }
-      // check if its a valid process
-      if (target_p == NULL) {
-        PROC_UNLOCK(target_p);
-        PROC_UNLOCK(this_p);
-        td->td_retval[0] = 0;
-      } else {
-        // count tickets per target process
-        FOREACH_THREAD_IN_PROC(target_p, td) {
-          thread_lock(td);	
-          total_tickets_per_targ += td->tickets;
-          thread_unlock(td);
-        }
-        
-        printf("before transfer: target process: %d\n", total_tickets_per_targ);
-        // check if this process can transfer
-        if (total_tickets_per_proc - tickets >= 1 && total_tickets_per_targ + tickets <= 100000) {
-          // transfer the tickets
-          sched_increaseTickets(target_p, tickets);
-          sched_decreaseTickets(this_p, tickets);
-          printf("after transfer: this process: %d\n", total_tickets_per_proc);
-          printf("after transfer: target process: %d\n", total_tickets_per_targ);
-	  PROC_UNLOCK(target_p);
-	  PROC_UNLOCK(this_p);
-        }
-      }
-    }
-  }
-return 0;
-}
+        int p_pid;
+        int tickets;
+        int total_tickets_per_proc = 0;
+        int total_tickets_per_targ = 0;
 
+        p_pid = args->p_pid;
+        tickets = args->tickets;
+
+        struct proc *this_p = td->td_proc; //1)defines the current process
+        PROC_LOCK(this_p);
+
+        FOREACH_THREAD_IN_PROC(this_p, td) {
+                thread_lock(td);
+                total_tickets_per_proc += td->tickets;
+                thread_unlock(td);
+        }
+
+        printf("before transfer: this process: %d\n", total_tickets_per_proc);
+
+        // check if the owner process is root process
+        if (this_p->p_ucred == 0) {
+                PROC_UNLOCK(this_p);
+                return 0;
+        }
+        // if gift(0,0);
+        if(p_pid == 0 && tickets == 0) {
+                td->td_retval[0] = total_tickets_per_proc;
+                PROC_UNLOCK(this_p);
+                return 0;
+        }
+        // check that tickets is a valid number
+        if (tickets > 0) {
+                struct proc *target_p = pfind(p_pid);
+                // check if its a valid process
+                if (target_p == NULL) {
+                        PROC_UNLOCK(this_p);
+                        return 0;
+                }
+                // check if the target process is root process
+                if (target_p->p_ucred == 0) {
+                        PROC_UNLOCK(this_p);
+                        PROC_UNLOCK(target_p);
+                        return 0;
+                }
+                // count tickets per target process
+                FOREACH_THREAD_IN_PROC(target_p, td) {
+                        thread_lock(td);
+                        total_tickets_per_targ += td->tickets;
+                        thread_unlock(td);
+                }
+
+                printf("before transfer: target process: %d\n", total_tickets_per_targ);
+                // check if this process can transfer
+                if (total_tickets_per_proc - tickets >= 1 && total_tickets_per_targ + tickets <= 100000) {
+                        // transfer the tickets
+                        sched_increaseTickets(target_p, tickets);
+                        sched_decreaseTickets(this_p, tickets);
+                        printf("after transfer: this process: %d\n", total_tickets_per_proc);
+                        printf("after transfer: target process: %d\n", total_tickets_per_targ);
+                        PROC_UNLOCK(target_p);
+                        PROC_UNLOCK(this_p);
+                }
+        }
+        return 0;
+}
